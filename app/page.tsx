f25122cfe8466be2d2bf85d01a9e040d350f2657
Abhineet <abhineetc9001@gmail.com>
2025-11-18 01:16:49 +0530

    changes in recent projects on grid card sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FLoatingNavbar";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from '../components/Grid';
-import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-gray-100 dark:bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[
-            {
-              name: "Home",
-              link: "/",
-              icon: <FaHome />,
-            },
-          ]}
+          navItems={navItems}
         />
         <Hero />
         <Grid />
